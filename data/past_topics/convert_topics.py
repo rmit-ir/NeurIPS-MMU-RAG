29319ec54db72e93eaacf06c457ae0f90f454bf8
Kun Ran <rankun203@gmail.com>
2025-09-16 23:11:59 +1000

    feat: add past topic files

---
+++ data/past_topics/convert_topics.py
@@ -0,0 +1,23 @@
+# /// script
+# dependencies = [
+#   "pandas",
+# ]
+# ///
+from pathlib import Path
+import pandas as pd
+
+work_dir = Path('data/past_topics/')
+d = work_dir
+do = work_dir / 'processed'
+do.mkdir(exist_ok=True)
+
+pd.read_json(d/'LiveRAG_LCD_Session1_Question_file.jsonl', lines=True) \
+  .rename(columns={'id': 'iid', 'question': 'query'}) \
+  .to_json(do/'LiveRAG_LCD_Session1_Question_file.jsonl', lines=True, orient='records')
+
+pd.read_json(d/'trec_rag_2025_queries.jsonl') \
+  .rename(columns={'id': 'iid', 'narrative': 'query'}) \
+  .to_json(do/'trec_rag_2025_queries.jsonl', lines=True, orient='records')
+
+pd.read_csv(d/'topics.rag24.test.tsv', sep='\t', names=['iid', 'query']) \
+  .to_json(do/'topics.rag24.test.jsonl', lines=True, orient='records')
