8b796ea2b07892ea71bff01bdd8aadda10583cfc
Kun Ran <rankun203@gmail.com>
2026-03-25 23:25:38 +1100

    chore: vanilla rag response

--- src/systems/vanilla_agent/vanilla_rag.py
+++ src/systems/vanilla_agent/vanilla_rag.py
@@ -181,7 +181,7 @@ class VanillaRAG(RAGInterface):
                 docs = truncate_docs(docs, self.retrieval_words_threshold)
                 docs = update_docs_sids(docs)
                 reranked_docs = len(docs)
-                yield inter_resp(f"""Search returned {total_docs}, identified {reranked_docs} relevant, truncated to {len(docs)} web pages.""", silent=False, logger=self.logger)
+                yield inter_resp(f"""Search returned {total_docs}, identified {reranked_docs} relevant, truncated to {len(docs)} web pages.\n\n""", silent=False, logger=self.logger)
 
                 yield inter_resp("Starting final answer\n\n", silent=False, logger=self.logger)
                 messages = build_llm_messages(
